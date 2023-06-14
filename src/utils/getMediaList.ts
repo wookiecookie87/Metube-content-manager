d261ea765d1cc4bd4ea9494ad221ab069ca99377
jung-wook <wookiecookie87@gmail.com>
2023-06-14 10:17:51 +0900

    [error] api function fix

--- src/utils/getMediaList.ts
+++ src/utils/getMediaList.ts
@@ -1,16 +1,11 @@
 export async function getMediaList() {
-  try {
-    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/video/list/1/12/created_at`,
-      {
-        cache: "no-store",
-      }
-    );
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_BASE_URL}/api/video/list/1/12/created_at`,
+    {
+      cache: "no-store",
+    }
+  );
 
-    const videoData = await response.json();
-    return videoData;
-  } catch (error) {
-    console.error("ERROR", error);
-    return [];
-  }
+  const videoData = await response.json();
+  return videoData;
 }
